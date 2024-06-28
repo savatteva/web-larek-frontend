1119e6ee378d30b86b92defc82671ecfc1574992
Polina Savateeva <polina.savateeva@geogracom.com>
2024-06-28 14:42:54 +0300

    trying to fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,3 @@
-export const API_URL = `${process.env.API_ORIGIN}/api/weblarek`;
-export const CDN_URL = `${process.env.API_ORIGIN}/content/weblarek`;
-
-export const settings = {
-
-};
+const API_ORIGIN = `https://larek-api.nomoreparties.co`
+export const API_URL = `${API_ORIGIN}/api/weblarek`;
+export const CDN_URL = `${API_ORIGIN}/content/weblarek`;
